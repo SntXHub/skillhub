06d88841e28d94a6f4078d7895de5d79266d699c
Santiago Gabriel Cabrera <santyx@gmail.com>
2025-06-23 02:42:16 -0300

    Avances en seguridad y usuarios

--- src/main/java/com/skillhub/skillhub/config/SecurityConfig.java
+++ src/main/java/com/skillhub/skillhub/config/SecurityConfig.java
@@ -22,12 +22,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/usuarios/registrar").permitAll()
-                .anyRequest().authenticated()
-            )
-            .httpBasic();
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/usuarios/registrar").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic();
 
         return http.build();
     }
